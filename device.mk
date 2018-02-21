cc8cc471d70a7b4af1fa62ac5a703ce58faee93f
bonuzzz <bonuzzz@gmail.com>
2018-02-21 16:03:30 +0300

    audio

--- device.mk
+++ device.mk
@@ -10,7 +10,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
