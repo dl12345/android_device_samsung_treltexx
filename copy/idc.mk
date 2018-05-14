4d403af663711dd9faa955c158c61d09b7d9df5d
DL <dl12345@github.com>
2018-05-15 05:03:39 +0700

    Remove Synaptics IDC files
    Change-Id: Id869e2549b41119b14359b3dc90dc09b96ce4245

--- copy/idc.mk
+++
@@ -1,5 +0,0 @@
-# idc
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:/system/usr/idc/Synaptics_HID_TouchPad.idc \
-    $(LOCAL_PATH)/configs/idc/Synaptics_RMI4_TouchPad_Sensor.idc:/system/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc
-
